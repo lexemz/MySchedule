b8f40c8ba8e6de2b52bb7175520c0d9635f222fa
Igor Buzykin <lexemz@gmail.com>
2022-04-25 15:34:46 +0300

    Чистка кода FormScheduleTableViewCell
    - Добавлены маркеры навигации
    - Методы установки констреинтов вынесены в расширение типа

--- MySchedule/ViewControllers/Schedule/Cells/FormScheduleTableViewCell.swift
+++ MySchedule/ViewControllers/Schedule/Cells/FormScheduleTableViewCell.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 class FormScheduleTableViewCell: UITableViewCell {
+    // MARK: - Public Properties
+
     static let id = "scheduleFormCell"
     static let height: CGFloat = 44
-    
+
+    // MARK: - UI Elements
+
     lazy var backgroundViewCell: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -18,35 +22,42 @@ class FormScheduleTableViewCell: UITableViewCell {
         view.backgroundColor = .systemBackground
         return view
     }()
-    
+
     lazy var cellLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
+
+    // MARK: - Cell LifeCycle
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         backgroundColor = .clear
     }
-    
-    override func layoutSubviews() {
-        setupConstraints()
-    }
-    
+
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
+    override func layoutSubviews() {
+        setupConstraints()
+    }
+}
+
+// MARK: - Setup Constraints
+
+extension FormScheduleTableViewCell {
     private func setupConstraints() {
         addSubview(backgroundViewCell)
         NSLayoutConstraint.activate([
             backgroundViewCell.topAnchor.constraint(equalTo: topAnchor),
             backgroundViewCell.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
             backgroundViewCell.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
-            backgroundViewCell.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -1)
+            backgroundViewCell.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -1)
         ])
-        
+
         addSubview(cellLabel)
         NSLayoutConstraint.activate([
             cellLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
