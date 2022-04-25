2e833e94675a07d8dc7a8c36e9412b49d4e91a18
Igor Buzykin <lexemz@gmail.com>
2022-04-25 15:28:33 +0300

    Конфигурация FormScheduleTableViewCell
    Настроено отображение ячейки

--- MySchedule/Model/Cells/FormScheduleTableViewCell.swift
+++ MySchedule/Model/Cells/FormScheduleTableViewCell.swift
@@ -9,12 +9,48 @@ import UIKit
 
 class FormScheduleTableViewCell: UITableViewCell {
     static let id = "scheduleFormCell"
+    static let height: CGFloat = 44
+    
+    lazy var backgroundViewCell: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 10
+        view.backgroundColor = .systemBackground
+        return view
+    }()
+    
+    lazy var cellLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .clear
+    }
+    
+    override func layoutSubviews() {
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupConstraints() {
+        addSubview(backgroundViewCell)
+        NSLayoutConstraint.activate([
+            backgroundViewCell.topAnchor.constraint(equalTo: topAnchor),
+            backgroundViewCell.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            backgroundViewCell.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            backgroundViewCell.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -1)
+        ])
+        
+        addSubview(cellLabel)
+        NSLayoutConstraint.activate([
+            cellLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            cellLabel.leadingAnchor.constraint(equalTo: backgroundViewCell.leadingAnchor, constant: 15)
+        ])
     }
 }
--- MySchedule/ViewControllers/Schedule/FormScheduleTableViewController.swift
+++ MySchedule/ViewControllers/Schedule/FormScheduleTableViewController.swift
@@ -7,21 +7,21 @@
 
 import UIKit
 
-enum ScheduleFormSectionsConfiguration: String, CaseIterable {
+enum ScheduleFormConfiguration: String, CaseIterable {
     case first
     case second
     case thrid
     case fourth
+    
+    static let cellsInSection: [Self: Int] = [
+        .first: 1,
+        .second: 2,
+        .thrid: 4,
+        .fourth: 1
+    ]
 }
 
 class FormScheduleTableViewController: UITableViewController {
-    
-    private let cells: [ScheduleFormSectionsConfiguration: [UITableViewCell]] = [
-        .first: [],
-        .second: [],
-        .thrid: [],
-        .fourth: []
-    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,32 +29,51 @@ class FormScheduleTableViewController: UITableViewController {
     }
     
     private func configureScreen() {
-        view.backgroundColor = .systemGray
+        view.backgroundColor = .systemBackground
         title = "Add Event"
         
         tableView.register(
             FormScheduleTableViewCell.self,
             forCellReuseIdentifier: FormScheduleTableViewCell.id
         )
+        
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .systemGray5
     }
-    
+}
+
+// MARK: - TableViewDataSourse & TableViewDelegate methods
+extension FormScheduleTableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
-        ScheduleFormSectionsConfiguration.allCases.count
+        ScheduleFormConfiguration.allCases.count
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let sectionKey = ScheduleFormSectionsConfiguration.allCases[section]
-        guard let availableCells = cells[sectionKey] else { return 0 }
-        return availableCells.count
+    override func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        let section = ScheduleFormConfiguration.allCases[section]
+        guard let availableCells = ScheduleFormConfiguration.cellsInSection[section] else { return 0 }
+        return availableCells
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(
             withIdentifier: FormScheduleTableViewCell.id,
             for: indexPath
         ) as! FormScheduleTableViewCell
         
-        
+        cell.cellLabel.text = "\(indexPath.row)"
         return cell
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        heightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
+        FormScheduleTableViewCell.height
     }
 }
