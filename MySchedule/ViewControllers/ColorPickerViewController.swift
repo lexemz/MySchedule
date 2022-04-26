20273a1590b93bfa42c4227f752282729e201fe5
Igor Buzykin <lexemz@gmail.com>
2022-04-26 17:31:45 +0300

    Добавлено 2 пустых экрана
    - TeachersViewController
    - ColorPickerViewController

---
+++ MySchedule/ViewControllers/ColorPickerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ColorPickerViewController.swift
+//  MySchedule
+//
+//  Created by Igor on 26.04.2022.
+//
+
+import UIKit
+
+class ColorPickerViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        title = "Color Picker"
+    }
+}
