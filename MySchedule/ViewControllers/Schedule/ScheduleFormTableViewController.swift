033e6a6ec06a32af8f280007019b18efd1f2933d
Igor Buzykin <lexemz@gmail.com>
2022-05-11 17:48:17 +0300

    Обновление ScheduleFormTableConfiguration

--- MySchedule/ViewControllers/Schedule/ScheduleFormTableViewController.swift
+++ MySchedule/ViewControllers/Schedule/ScheduleFormTableViewController.swift
@@ -52,15 +52,6 @@ class ScheduleFormTableViewController: UITableViewController {
     private let sections = ScheduleFormTableConfiguration.allCases
     
     // MARK: - ViewController Lifecycle
-
-    override init(style: UITableView.Style) {
-        super.init(style: style)
-    }
-    
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
