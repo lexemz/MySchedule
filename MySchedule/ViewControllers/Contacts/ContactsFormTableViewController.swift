4bc6b29dbd9a44f17c31659acd3045f797f21101
Igor Buzykin <lexemz@gmail.com>
2022-05-13 14:28:55 +0300

    Реализована форма для создания контактов

--- MySchedule/ViewControllers/Contacts/ContactsFormTableViewController.swift
+++ MySchedule/ViewControllers/Contacts/ContactsFormTableViewController.swift
@@ -7,25 +7,124 @@
 
 import UIKit
 
-class ContactsFormTableViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        view.backgroundColor = .systemGray5
-        title = "Teachers"
-    }
+enum ContactsFormTableConfiguration: String, CaseIterable {
+    case image = ""
+    case name = "Name"
+    case phoneAndMail = "Phone and Mail"
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var cellsTitles: [String] {
+        switch self {
+        case .image:
+            return [""]
+        case .name:
+            return ["Name"]
+        case .phoneAndMail:
+            return ["Phone", "Mail"]
+        }
     }
-    */
-
 }
 
+class ContactsFormTableViewController: UITableViewController {
+    // MARK: - Private Properties
+
+    private let sections = ContactsFormTableConfiguration.allCases
+    
+    // MARK: - Override Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureScreen()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func configureScreen() {
+        view.backgroundColor = .systemGray5
+        title = "Add Contact"
+        
+        tableView.register(
+            TextFieldTableViewCell.self,
+            forCellReuseIdentifier: TextFieldTableViewCell.id
+        )
+        
+        tableView.keyboardDismissMode = .onDrag
+        let tapGesture = UITapGestureRecognizer(
+            target: tableView,
+            action: #selector(UIView.endEditing(_:))
+        )
+        tableView.addGestureRecognizer(tapGesture)
+        tapGesture.cancelsTouchesInView = false
+    }
+}
+
+// MARK: - TableViewDataSource & TableViewDelegate
+
+extension ContactsFormTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        sections.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        sections[section].cellsTitles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        sections[section].rawValue
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellTitle = sections[indexPath.section].cellsTitles[indexPath.row]
+        
+        switch indexPath {
+        case [0, 0]:
+            return UITableViewCell()
+        case [1, 0]:
+            let textfieldCell = tableView.dequeueReusableCell(
+                withIdentifier: TextFieldTableViewCell.id
+            ) as! TextFieldTableViewCell
+            textfieldCell.configure(indexPath, placeHolder: cellTitle)
+            textfieldCell.delegate = self
+            return textfieldCell
+        case [2,0], [2,1]:
+            let textfieldCell = tableView.dequeueReusableCell(
+                withIdentifier: TextFieldTableViewCell.id
+            ) as! TextFieldTableViewCell
+            textfieldCell.configure(indexPath, placeHolder: cellTitle)
+            textfieldCell.delegate = self
+            return textfieldCell
+        default:
+            return UITableViewCell()
+        }
+    }
+}
+
+// MARK: - TextFieldTableViewCellDelegate
+
+extension ContactsFormTableViewController: TextFieldTableViewCellDelegate {
+    func textFieldShouldReturn(indexPath: IndexPath) {
+        let nextRowIndexPath = IndexPath(
+            row: indexPath.row + 1,
+            section: indexPath.section
+        )
+        if let nextCell = tableView.cellForRow(at: nextRowIndexPath) as? TextFieldTableViewCell {
+            nextCell.focus()
+            return
+        }
+        
+        let nextGroupIndexPath =  IndexPath(
+            row: indexPath.row,
+            section: indexPath.section + 1
+        )
+        if let nextCell = tableView.cellForRow(at: nextGroupIndexPath) as? TextFieldTableViewCell {
+            nextCell.focus()
+            return
+        }
+        
+        let currentCell = tableView.cellForRow(at: indexPath) as? TextFieldTableViewCell
+        currentCell?.unfocus()
+    }
+    
+    func textFieldDidEndEditing(value: String?, at indexPath: IndexPath) {
+        Logger.debug(value)
+    }
+}
